0265501f9a7fa07b3fd59d3b4922d65d5b381b54
ferrlswheelsticket <649599725@qq.com>
2016-10-17 17:01:20 +0800

    add Command Pattern

---
+++ app/src/main/java/com/admin/designpattern/Command/Command.java
@@ -0,0 +1,11 @@
+package com.admin.designpattern.Command;
+
+/**
+ * Created by Administrator on 2016/10/17 0017.
+ */
+public interface Command {
+
+
+    void execute(String command);
+
+}
---
+++ app/src/main/java/com/admin/designpattern/Command/Engineer.java
@@ -0,0 +1,21 @@
+package com.admin.designpattern.Command;
+
+/**
+ * Created by Administrator on 2016/10/17 0017.
+ */
+public class Engineer implements Command {
+    @Override
+    public void execute(String command) {
+        switch (command){
+            case "command 1":
+                break;
+            case "command 2":
+                break;
+            case "command 3":
+                break;
+            case "command 4":
+                break;
+
+        }
+    }
+}
